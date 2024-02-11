2e3fce8cfbbfb0850ada9a197f84fdbe6ba0d59d
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-02-11 12:15:32 +0100

    fix: visibilities

--- core/src/main/scala/it/unibo/scafi/xc/engine/context/exchange/ExchangeCalculusContext.scala
+++ core/src/main/scala/it/unibo/scafi/xc/engine/context/exchange/ExchangeCalculusContext.scala
@@ -1,29 +1,15 @@
 package it.unibo.scafi.xc.engine.context.exchange
 
-import it.unibo.scafi.xc.engine.context.Context
 import it.unibo.scafi.xc.engine.context.common._
-import it.unibo.scafi.xc.engine.network.{ Export, Import }
-import it.unibo.scafi.xc.language.semantics.exchange.ExchangeCalculusSemantics
+import it.unibo.scafi.xc.engine.network.Import
 
 class ExchangeCalculusContext[Id](
-    override val self: Id,
-    override val inboundMessages: Import[Id, InvocationCoordinate, Any],
-) extends Context[Id, InvocationCoordinate, Any]
-    with ExchangeCalculusSemantics
-    with NValuesSemantics
-    with ConstructsSemantics
-    with StackSemantics
-    with MessageSemantics
-    with InboundMessagesSemantics
-    with OutboundMessagesSemantics:
-  override type DeviceId = Id
-  override type Envelope = Any
+    self: Id,
+    inboundMessages: Import[Id, InvocationCoordinate, Any],
+) extends AbstractExchangeCalculusContext[Id, Any](self, inboundMessages):
 
   override protected def open[T](a: Any): T = a match
     case t: T @unchecked => t
     case _ => throw new ClassCastException(s"Cannot cast $a to requested type")
 
   override protected def close[T](a: T): Any = a
-
-  override def messages: Export[Id, InvocationCoordinate, Any] = outboundMessages
-end ExchangeCalculusContext
