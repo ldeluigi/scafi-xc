ac4f3a8d8ebc92134821cfbeb25b85e45a7c6012
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-02-20 23:13:56 +0100

    docs: add docs to Engine.scala

--- core/src/main/scala/it/unibo/scafi/xc/engine/Engine.scala
+++ core/src/main/scala/it/unibo/scafi/xc/engine/Engine.scala
@@ -3,6 +3,28 @@ package it.unibo.scafi.xc.engine
 import it.unibo.scafi.xc.engine.context.{ Context, ContextFactory }
 import it.unibo.scafi.xc.engine.network.{ Export, Network }
 
+/**
+ * The engine is responsible for linking a context with a network and a program and handling the program execution for
+ * every cycle.
+ * @param net
+ *   the network interface
+ * @param factory
+ *   the context factory, used to create a new context for every cycle
+ * @param program
+ *   the program to be executed every cycle
+ * @tparam DeviceId
+ *   the type of the device id
+ * @tparam Result
+ *   the type of the result of the program
+ * @tparam Token
+ *   the type of the token
+ * @tparam Value
+ *   the type of the value
+ * @tparam N
+ *   the type of the network
+ * @tparam C
+ *   the type of the context
+ */
 class Engine[
     DeviceId,
     Result,
@@ -17,19 +39,40 @@ class Engine[
 ):
 
   private def round(): AggregateResult =
-    given c: C = factory.create(net)
+    given context: C = factory.create(net)
     val result: Result = program
-    val outMessages = c.outboundMessages
+    val outMessages = context.outboundMessages
     net.send(outMessages)
-    AggregateResult(result, c.inboundMessages, outMessages)
+    AggregateResult(result, context.inboundMessages, outMessages)
 
+  /**
+   * Executes a single cycle of the program.
+   * @return
+   *   the result of the program
+   */
   def cycle(): Result = round().result
 
-  def cycleWhile(p: AggregateResult => Boolean): Result =
-    var ar = round()
-    while p(ar) do ar = round()
-    ar.result
+  /**
+   * Executes the program until a condition is no longer satisfied.
+   * @param predicate
+   *   the condition to be checked at the end of every cycle
+   * @return
+   *   the result of the program after the last cycle
+   */
+  def cycleWhile(predicate: AggregateResult => Boolean): Result =
+    var aggregateResult = round()
+    while predicate(aggregateResult) do aggregateResult = round()
+    aggregateResult.result
 
+  /**
+   * Compact representation of different aspects of an aggregate computation round/cycle.
+   * @param result
+   *   the result of the computation
+   * @param incomingMessages
+   *   the messages received from the network before the computation
+   * @param outgoingMessages
+   *   the messages sent to the network after the computation
+   */
   case class AggregateResult(
       result: Result,
       incomingMessages: Export[DeviceId, Token, Value],
