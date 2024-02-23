e3321eceb3908517973dcc8994c781bbbff58114
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-02-23 09:53:23 +0100

    test: improve readability

--- tests/src/test/scala/it/unibo/scafi/xc/tests/acceptance/GradientWithObstacleTest.scala
+++ tests/src/test/scala/it/unibo/scafi/xc/tests/acceptance/GradientWithObstacleTest.scala
@@ -8,12 +8,18 @@ import it.unibo.scafi.xc.language.libraries.All.{ *, given }
 
 class GradientWithObstacleTest extends AcceptanceTest with GridNetwork:
   override type TestProgramResult = Double
-  val epsilon = 0.0001
+  val epsilon: Double = 0.0001
+  val obstacleGradient: Double = Double.PositiveInfinity
   override def rows: Int = 10
   override def columns: Int = 10
   override def ticks: Int = 1600
   def isSource(id: PositionInGrid): Boolean = id.row == 0 && id.col == 0
   def isObstacle(id: PositionInGrid): Boolean = id.row > 0 && id.col == 4
+
+  def expectedGradient(id: PositionInGrid): Double =
+    if isObstacle(id) then obstacleGradient
+    else if id.col < 4 || id.row == 0 then Math.max(id.row, id.col).toDouble
+    else Math.max(id.row, id.col - 4).toDouble + 4
 
   // Network:
   // s * * * * * * * * *
@@ -32,18 +38,15 @@ class GradientWithObstacleTest extends AcceptanceTest with GridNetwork:
 
   override def program(using TestProgramContext): Double =
     val round = rep(0)(_ + 1)
-    branch(isObstacle(self) && round >= 200)(Double.PositiveInfinity)(distanceTo(isSource(self), 1.0))
+    branch(isObstacle(self) && round >= 200)(obstacleGradient)(distanceTo(isSource(self), 1.0))
 
   "The gradient" should "never be calculated for the obstacle" in:
     results
       .filter(kv => isObstacle(kv._1))
       .foreach: (id, value) =>
-        value shouldBe Double.PositiveInfinity
+        value shouldBe obstacleGradient
 
   it should "be calculated correctly with obstacles" in:
-    results
-      .filter(kv => !isObstacle(kv._1))
-      .foreach: (id, value) =>
-        if id.col < 4 || id.row == 0 then value shouldBe Math.max(id.row, id.col).toDouble +- epsilon
-        else value shouldBe Math.max(id.row, id.col - 4).toDouble + 4 +- epsilon
+    results.foreach: (id, value) =>
+      value shouldBe expectedGradient(id) +- epsilon
 end GradientWithObstacleTest
