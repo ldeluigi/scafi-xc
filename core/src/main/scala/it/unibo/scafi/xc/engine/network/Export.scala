a60ea9727d0c47cf46d824136b90368fb64710e0
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-03-04 01:11:05 +0100

    docs: fix import and export

--- core/src/main/scala/it/unibo/scafi/xc/engine/network/Export.scala
+++ core/src/main/scala/it/unibo/scafi/xc/engine/network/Export.scala
@@ -7,8 +7,6 @@ import it.unibo.scafi.xc.collections.MapWithDefault
  * corresponding. The default value provides a tree to send to a device that is not present in the map.
  * @tparam DeviceId
  *   the type of the device id
- * @tparam Token
- *   the type of the tokens that compose a path to a value in the tree
  * @tparam Value
  *   the type of the values in the tree
  */
--- core/src/main/scala/it/unibo/scafi/xc/engine/network/Import.scala
+++ core/src/main/scala/it/unibo/scafi/xc/engine/network/Import.scala
@@ -6,8 +6,6 @@ package it.unibo.scafi.xc.engine.network
  *
  * @tparam DeviceId
  *   the type of the device id
- * @tparam Token
- *   the type of the tokens that compose a path to a value in the tree
  * @tparam Value
  *   the type of the values in the tree
  */
